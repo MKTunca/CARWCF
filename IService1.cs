61d2e5491b783bdbbe0293ef6bd3b162982315e3
MKTunca <91753517+MKTunca@users.noreply.github.com>
2021-12-27 00:28:27 +0300

    Add files via upload

---
+++ IService1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace CarWCF
+{
+    
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        List<ARAC> araclar();
+       
+    }
+
+
+    
+    
+}
---
+++ Service1.svc.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace CarWCF
+{
+    
+    public class Service1 : IService1
+    {
+        RentACarEntities baglan = new RentACarEntities();
+        public List<ARAC> araclar()
+        {
+            var model = baglan.ARAC.ToList();
+
+            return model;
+
+        }
+    }
+}
